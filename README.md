406d8712401ecad1f112564eb48fc5d63ce5bd40
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-06-17 12:09:54 +0200

    Cambios inciales

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 P3 for HTML &amp; CSS I
 
-## Cambios
+## Ejecución
+
+## Publicaciones
+
+P2 en servidor final
+https://isaacblanco.com/UOC/MASTER/hc1/p2/index.html
+
+P3 en servidor final
+https://isaacblanco.com/UOC/MASTER/hc1/p3/index.html
+
+GitHub:
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -21,3 +21,44 @@
     });
   }
 })();
+
+document.addEventListener("DOMContentLoaded", function () {
+  let lazyImages = [].slice.call(document.querySelectorAll("img[data-src]"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function (
+      entries,
+      observer
+    ) {
+      entries.forEach(function (entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.removeAttribute("data-src");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+
+    lazyImages.forEach(function (lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  } else {
+    // Navegadores que no soportan IntersectionObserver
+    let lazyLoad = function () {
+      lazyImages.forEach(function (lazyImage) {
+        if (
+          lazyImage.getBoundingClientRect().top < window.innerHeight &&
+          lazyImage.getBoundingClientRect().bottom > 0
+        ) {
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.removeAttribute("data-src");
+        }
+      });
+    };
+
+    lazyLoad();
+    window.addEventListener("scroll", lazyLoad);
+    window.addEventListener("resize", lazyLoad);
+  }
+});
